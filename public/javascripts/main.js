caa28811ebd4dff83e43e3b06af7220765fa4d1f
Jayjaybunce <jayjaybunce>
2020-03-02 08:45:38 -0700

    Adds portion of samepage post editing

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -2,7 +2,10 @@ let commentButtonEls = document.querySelectorAll('.comment-button')
 commentButtonEls.forEach(el=>{
     el.addEventListener('click',handleClick)
 })
-
+let editButtonEls = document.querySelectorAll('.edit-button')
+editButtonEls.forEach(el=>{
+    el.addEventListener('click',handleEdit)
+})
 function handleClick(evt){
     let postId = evt.target.parentElement.parentElement.parentElement.getAttribute('data-id')
     let commentEl = document.querySelectorAll(`[data-id="${postId}"]`)[2]
@@ -27,6 +30,33 @@ function handleClick(evt){
             commentEl.style.display ='none'
         }
         
+
+}
+
+function handleEdit(evt){
+    let postContentEl = evt.target.parentElement.parentElement.parentElement
+    let pId = postContentEl.getAttribute('data-id')
+    let postContent = evt.target.parentElement.parentElement.parentElement.querySelector('.post-p').textContent
+    let postElement = evt.target.parentElement.parentElement.parentElement.querySelector('.post-p')
+    // where post content lives evt.target.parentElement.parentElement.parentElement.querySelector('.post-p')
+    postElement.style.display = 'none'
+    let textEl = document.createElement('textarea')
+    let updateButtonForm = document.createElement('form')
+    let updateButtonEl = document.createElement('button')
+    updateButtonEl.setAttribute('type','submit')
+    updateButtonEl.setAttribute('class','update-post-button')
+    updateButtonEl.textContent = 'Update'
+    updateButtonForm.setAttribute('action',`/posts/${pId}?_method=PUT`)
+    updateButtonForm.setAttribute('method','POST')
+    updateButtonForm.setAttribute('class','update-post-form')
+    textEl.textContent = postContent.trim()
+    textEl.setAttribute('class','edit-post-textarea')
+    updateButtonForm.appendChild(textEl)
+    postContentEl.appendChild(updateButtonForm)
+    updateButtonForm.appendChild(updateButtonEl)
+    
+    
+
 
 }
 
